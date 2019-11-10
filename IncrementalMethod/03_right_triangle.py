fc36c29c75928414c6eb6a17d4072593792eb9cb
Ken Huang <kenhuang2019iii@gmail.com>
2019-11-10 14:22:11 +0800

    Update 03_right_triangle.py

--- IncrementalMethod/03_right_triangle.py
+++ IncrementalMethod/03_right_triangle.py
@@ -1,3 +1,7 @@
-l = 5 # 單行長度
-for i in range(l, 0, -1):
-    print('@'*i)
+def draw_right_triangle(l):
+    for i in range(l, 0, -1):
+        print('@'*i)
+
+if __name__ == "__main__":
+    user_input = input("Enter a number as side length:\n").strip()
+    draw_right_triangle(int(user_input))
